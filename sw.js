a3903fea2f6b00965366fd07739272cf71b2dc62
Michael M <Schnark@users.noreply.github.com>
2019-02-01 10:17:47 +0100

    Fix service worker

--- sw.js
+++ sw.js
@@ -2,7 +2,8 @@
 (function (worker) {
 "use strict";
 
-var VERSION = 'v2.6',
+var PREFIX = 'sudoku',
+	VERSION = '2.6',
 	FILES = [
 		'app.css',
 		'index.html',
@@ -23,7 +24,7 @@ var VERSION = 'v2.6',
 
 worker.addEventListener('install', function (e) {
 	e.waitUntil(
-		caches.open(VERSION).then(function (cache) {
+		caches.open(PREFIX + ':' + VERSION).then(function (cache) {
 			return cache.addAll(FILES);
 		})
 	);
@@ -33,7 +34,7 @@ worker.addEventListener('activate', function (e) {
 	e.waitUntil(
 		caches.keys().then(function (keys) {
 			return Promise.all(keys.map(function (key) {
-				if (key !== VERSION) {
+				if (key.indexOf(PREFIX + ':') === 0 && key !== PREFIX + ':' + VERSION) {
 					return caches.delete(key);
 				}
 			}));
